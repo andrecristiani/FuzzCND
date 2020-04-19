1fa2b00fe63bdec8eacc5f8da71c04e86831467a
andrecristiani <aluis02@hotmail.com>
2020-04-18 22:33:08 -0300

    fix: adding parameters of SPFMiC in root

--- src/main/java/FuzzyProject/FuzzyDecisionTree.java
+++ src/main/java/FuzzyProject/FuzzyDecisionTree.java
@@ -4,15 +4,13 @@ import FuzzyProject.FuzzyDT.Fuzzy.CombinatoricException;
 import FuzzyProject.FuzzyDT.Models.ComiteArvores;
 import FuzzyProject.FuzzyND.FaseOffline;
 import FuzzyProject.FuzzyND.FaseOnline;
-import FuzzyProject.FuzzyND.Utils.LineChart_AWT;
-import org.jfree.ui.RefineryUtilities;
 
 import java.io.File;
 import java.io.IOException;
 
 public class FuzzyDecisionTree {
     public static void main(String[] args) throws IOException, CombinatoricException, Exception {
-        int numCjtos = 7;
+        int numCjtos = 6;
         String dataset = "moa";
         String taxaPoda = "25";
         String caminho = "";
@@ -21,30 +19,18 @@ public class FuzzyDecisionTree {
         int tComite = 6;
         int tChunk = 2000;
         int K = 50;
-        int T = 2000;
+        int tDesconhecidos = 50;
+        double phi = 0.5;
+        double alpha = 1;
+        double betha = 1;
+        double todasTipMax = 0.95;
+        double adaptadorTheta = 0.90;
 
         FaseOffline faseOffline = new FaseOffline();
-        ComiteArvores comite = faseOffline.inicializarKMeans(dataset, caminho, taxaPoda, numCjtos, tComite, tChunk, K);
+        ComiteArvores comite = faseOffline.inicializarFuzzyCMeans(dataset, caminho, taxaPoda, numCjtos, tComite, tChunk, K, 2, alpha, betha);
 
         FaseOnline faseOnline = new FaseOnline(2,2, K, 2, 2, 1,1, 5, 2000);
-        faseOnline.inicializar(caminho, dataset, comite, T, 40);
-
-//        ConverterUtils.DataSource source;
-//        Instances data;
-//        try {
-//            source = new ConverterUtils.DataSource(caminho + dataset + "-test.arff");
-//            data = source.getDataSet();
-//            List<Exemplo> exemplosRotulados = new ArrayList<>();
-//            for(int i=0; i<data.size(); i++) {
-//                Instance ins = data.get(i);
-//                Exemplo exemplo = new Exemplo(ins.toDoubleArray(), true);
-//                exemplosRotulados.add(exemplo);
-//            }
-//            comite.treinaNovaArvore(exemplosRotulados, tChunk, K);
-//            System.out.println("Terminou");
-//        } catch (Exception ex) {
-//            System.err.println("Erro:" + ex);
-//        }
+        faseOnline.inicializarFuzzyCMeans(caminho, dataset, comite, tDesconhecidos, 40, phi, todasTipMax, adaptadorTheta);
     }
 }
 
